b14605f94eda6c7ac8d93e2f22ed974487eaaadb
dodinhduy2k3 <dodinhduy203@gmail.com>
2023-09-20 23:33:01 +0700

    hiện thi giao diện

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,16 @@
-
+const Product = require("../../models/product.model")
 module.exports.index= async(req, res) => {
-   res.render('client/page/products/index.pug', {
-        titlepage: "Danh Sách Sản Phẩm "
+    const products = await Product.find();
+        
+        console.log(products)
+        const newproduct = products.map(item=>{
+            item.priceNew= ((item.price * (100 - item.discountPercentage))/100).toFixed(0)
+            return item
+        })
+
+        console.log(newproduct)
+    res.render('client/page/products/index.pug', {
+        titlepage: "Danh Sách Sản Phẩm ",
+        products: products
     });
 }
